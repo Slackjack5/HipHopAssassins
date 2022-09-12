32b9ec397f8a5d1a8c1ba0632142846258451be5
Nycelly <slackjack5@gmail.com>
2022-09-11 22:46:31 -0400

    WiP Turn-Based Menu
    Working on Attack Button with Limp Attack choice

--- Assets/Scripts/User Interface/UIManager.cs
+++ Assets/Scripts/User Interface/UIManager.cs
@@ -9,27 +9,30 @@ using Vector2 = System.Numerics.Vector2;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject actionBlock;
+    [SerializeField] private GameObject userInterface;
+    private GameObject actionBlock;
     protected State currentMenu;
-    private Vector2 menuNavigation;
+    protected int menuNumber;
+    private int menuNavigation;
     protected enum State
     {
         Home,
         Attack,
         Magic,
         Items,
-        Flee
+        Flee,
+        EnemyTurn,
     }
 
     private void Start()
     {
-        menuNavigation.X = 0;
-        menuNavigation.Y = 1;
+        menuNavigation = 0;
     }
 
     protected void ChangeState(State state)
     {
         currentMenu = state;
+        //Hide UI
     }
     // Update is called once per frame
     void Update()
@@ -37,19 +40,27 @@ public class UIManager : MonoBehaviour
         switch (currentMenu)
         {
             case State.Home:
+                actionBlock = userInterface.transform.GetChild(0).gameObject;
+                menuNumber = 1;
                 maneuverMenu();
-                Debug.Log("Menu Location:"+menuNavigation.X+","+menuNavigation.Y);
+                HomeButtons();
                 break;
             case State.Attack:
-
+                actionBlock = userInterface.transform.GetChild(1).GetChild(0).gameObject;
+                menuNumber = 2;
+                maneuverMenu();
+                AttackButtons();
                 break;
             case State.Magic:
-
+                Debug.Log("In Magic State");
                 break;
             case State.Items:
-
+                Debug.Log("In Items State");
                 break;
             case State.Flee:
+                Debug.Log("In Flee State");
+                break;
+            case State.EnemyTurn:
 
                 break;
         }
@@ -57,60 +68,139 @@ public class UIManager : MonoBehaviour
 
     private void maneuverMenu()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            menuNavigation.Y -= 1;
+            menuNavigation -= 1;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            menuNavigation.X -= 1;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            menuNavigation.X += 1;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            menuNavigation.Y += 1;
+            menuNavigation += 1;
         }
         
         //Set for a Max size of 2
-        if (menuNavigation.X > 1) { menuNavigation.X = 1; }
-        else if (menuNavigation.X < 0) { menuNavigation.X = 0; }
+        if (menuNavigation > actionBlock.transform.childCount-1) { menuNavigation = actionBlock.transform.childCount-1; }
+        else if (menuNavigation < 0) { menuNavigation = 0; }
 
-        if (menuNavigation.Y > 1) { menuNavigation.Y = 1; }
-        else if (menuNavigation.Y < 0) { menuNavigation.Y = 0; }
-        
         //Current Menu Player is Hovering Over
-        if (menuNavigation.X == 0 && menuNavigation.Y == 1) //Attack
+        if (menuNavigation == 0) //Attack
         {
             resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Attack);
+            }
             actionBlock.transform.GetChild(0).GetComponent<Image>().color=Color.red;
+            
         }
-        else if (menuNavigation.X == 1 && menuNavigation.Y == 1) //Magic
+        else if (menuNavigation==1) //Magic
         {
             resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Magic);
+            }
             actionBlock.transform.GetChild(1).GetComponent<Image>().color=Color.red;
         }
-        else if (menuNavigation.X == 0 && menuNavigation.Y == 0) //Items
+        else if (menuNavigation==2) //Items
         {
             resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Items);
+            }
             actionBlock.transform.GetChild(2).GetComponent<Image>().color=Color.red;
         }
-        else if (menuNavigation.X == 1 && menuNavigation.Y == 0) //Flee
+        else if (menuNavigation==3) //Flee
         {
             resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Flee);
+            }
+            actionBlock.transform.GetChild(3).GetComponent<Image>().color=Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuNavigation = 0;
+            resetBlocks();
+            ChangeState(State.Home);
+        }
+    }
+
+    private void HomeButtons()
+    {
+        //Current Menu Player is Hovering Over
+        if (menuNavigation == 0) //Attack
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Attack);
+            }
+            actionBlock.transform.GetChild(0).GetComponent<Image>().color=Color.red;
+            
+        }
+        else if (menuNavigation==1) //Magic
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Magic);
+            }
+            actionBlock.transform.GetChild(1).GetComponent<Image>().color=Color.red;
+        }
+        else if (menuNavigation==2) //Items
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Items);
+            }
+            actionBlock.transform.GetChild(2).GetComponent<Image>().color=Color.red;
+        }
+        else if (menuNavigation==3) //Flee
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                ChangeState(State.Flee);
+            }
             actionBlock.transform.GetChild(3).GetComponent<Image>().color=Color.red;
         }
     }
 
+    private void AttackButtons()
+    {
+        //Current Menu Player is Hovering Over
+        if (menuNavigation == 0) //Attack
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                //Send Attack Data to combat Manager
+            }
+            actionBlock.transform.GetChild(0).GetComponent<Image>().color=Color.red;
+            
+        }
+        //Current Menu Player is Hovering Over
+        if (menuNavigation == 1) //Attack
+        {
+            resetBlocks();
+            if (Input.GetKeyDown("space")) 
+            {
+                //Send Attack Data to combat Manager
+            }
+            actionBlock.transform.GetChild(0).GetComponent<Image>().color=Color.red;
+            
+        }
+    }
+    
     private void resetBlocks()
     {
-        actionBlock.transform.GetChild(0).GetComponent<Image>().color=Color.white;
-        actionBlock.transform.GetChild(1).GetComponent<Image>().color=Color.white;
-        actionBlock.transform.GetChild(2).GetComponent<Image>().color=Color.white;
-        actionBlock.transform.GetChild(3).GetComponent<Image>().color=Color.white;
-
+        foreach (Transform child in actionBlock.transform)
+        {
+            child.GetComponent<Image>().color=Color.white;
+        }
     }
 
     private void FixedUpdate()
